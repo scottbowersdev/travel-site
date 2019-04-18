4ad86c2afcd284f2815579c6479e195386f7dbfc
Scott Bowers <hi@scottbowers.dev>
2019-04-18 11:18:07 +0100

    Latest files from course

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
-var gulp = require('gulp');
-var watch = require('gulp-watch');
+var gulp          = require('gulp');
+var watch         = require('gulp-watch');
+var postcss       = require('gulp-postcss');
+var autoprefixer  = require('autoprefixer');
+var cssvars       = require('postcss-simple-vars');
+var nested        = require('postcss-nested');
 
 gulp.task('default', function() {
 
@@ -15,7 +19,9 @@ gulp.task('html', function() {
 
 gulp.task('styles', function() {
 
-  console.log('Imagine SASS or post CSS tasks running here');
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles/'));
 
 });
 
